33d59b456b7bc57f6bcff7d0d42abdc025ae87a5
invektiv369 <invektiv369@hotmail.com>
2023-06-07 03:09:09 +0300

    івав

--- Exercises for begginners/Ex.1_Character Input.py
+++ Exercises for begginners/Ex.1_Character Input.py
@@ -2,7 +2,14 @@
 ##Print out a message addressed to them that tells them the year that they will turn 100 years old.
 ##Створіть програму, яка просить користувача ввести своє ім’я та вік. 
 ##Роздрукуйте повідомлення, адресоване їм, із зазначенням року, коли їм виповниться 100 років.
-name = input("What is your name: ")
-age = int(input("How old are you: "))
-year = 2023 - age + 100
-print(name + ", you will be 100 years old in the year " + str(year))
+language = int(input("EN(1)/UA(2):"))
+if language == 1:
+    name = input("What is your name: ")
+    age = int(input("How old are you: "))
+    year = 2023 - age + 100
+    print(name + ", you will be 100 years old in the year " + str(year))
+else:
+    name = input("Як тебе звати?: ")
+    age = int(input("Скільки тобі років?: "))
+    year = 2023 - age + 100
+    print(name + ", тобі буде 100 років у " + str(year) + " році")
